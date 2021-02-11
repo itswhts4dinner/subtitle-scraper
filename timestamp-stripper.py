21c74fb050a7aa8c3b15181614bee6de3c9c1927
itswhts4dinner <jmclaughl@gmail.com>
2021-02-11 17:43:30 -0500

    Update timestamp-stripper.py
    Added the ability to
    1)specify a directory,
    2)search for all .vtt files in it,
    3)convert them to .txt, and strip the time stamps

--- timestamp-stripper.py
+++ timestamp-stripper.py
@@ -1,8 +1,18 @@
 import sys
 import os
 
-bad_words = ['-->', 'WEBVTT']
-with open('oldfile.vtt') as oldfile, open('newfile.txt', 'w') as newfile:
-    for line in oldfile:
-        if not any(bad_word in line for bad_word in bad_words):
-            newfile.write(line)
+#ask for path to ccdirectory
+ccdirectory = input("Enter the path to the directory to translate:\n")
+os.chdir(ccdirectory)
+###
+#Get file folder and files#
+for root,_,files in os.walk(ccdirectory):
+    for file in files:
+        if file.endswith(".vtt"):
+            #specify the text to remove
+            bad_words = ['-->', 'WEBVTT']
+            new_file_name = str(os.path.join(root, file)) + '.txt'
+            with open(os.path.join(root,file)) as oldfile, open(new_file_name, 'w') as newfile:
+                for line in oldfile:
+                    if not any(bad_word in line for bad_word in bad_words):
+                        newfile.write(line)
