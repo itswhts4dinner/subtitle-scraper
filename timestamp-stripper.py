515824259d5595bf806ca4bf250b74f298441e64
itswhts4dinner <jmclaughl@gmail.com>
2021-02-11 17:49:37 -0500

    Update timestamp-stripper.py
    Removed the .vtt extension from the new file name so now it only reads .txt
    
    Also commented the code.

--- timestamp-stripper.py
+++ timestamp-stripper.py
@@ -11,8 +11,15 @@ for root,_,files in os.walk(ccdirectory):
         if file.endswith(".vtt"):
             #specify the text to remove
             bad_words = ['-->', 'WEBVTT']
-            new_file_name = str(os.path.join(root, file)) + '.txt'
+            #get the name of the new file
+            new_file_name = str(os.path.join(root, file))
+            #strip the .vtt ending from the file and append .tex
+            new_file_name = os.path.splitext(new_file_name)[0]+".txt"
+            #open each file in the directory one by one
             with open(os.path.join(root,file)) as oldfile, open(new_file_name, 'w') as newfile:
+                #examine each line of the file
                 for line in oldfile:
+                    #Look and see if there are any "bad_words", remove them if they're there
                     if not any(bad_word in line for bad_word in bad_words):
+                        #print each "bad_word" free line to the file.
                         newfile.write(line)
